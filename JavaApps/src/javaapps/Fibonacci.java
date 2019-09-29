0540d9b3b5c067e2ff6a37bcab3dfd34ed448ac4
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-09-29 18:48:21 +0100

    1

---
+++ JavaApps/src/javaapps/Fibonacci.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapps;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author barcz
+ */
+public class Fibonacci {
+    
+    private int lnumber;
+    private int fnumber = 1;
+    private int nextnumber;
+    private int addnumber = 1;
+    private int exitloop=0;
+    public void runF(){
+        System.out.println("Fibonacci series, e.g. 1 1 2 3 5 8 13 ... . up to a given number");
+        do {
+        Scanner getnumber = new Scanner(System.in);
+        System.out.println("Please give the last number max 50 :");
+        if (getnumber.hasNext()){
+                lnumber = getnumber.nextInt();
+                exitloop = 1;
+                System.out.println("i am here");
+                } else {
+                    exitloop = 0;
+                    System.out.println("here");
+                        }
+       
+        }while((lnumber >50) && (exitloop != 1));
+        System.out.println("your number:"+lnumber);
+        System.out.println("");
+        for (int i= 0 ; i<(lnumber-2); i++){
+            if (i == 0) {
+                System.out.print("1 "+fnumber);
+                        }
+            nextnumber = fnumber + addnumber;
+            System.out.print(" "+nextnumber);
+            addnumber = fnumber;
+            fnumber = nextnumber;
+        }
+        
+    }
+    
+}
---
+++ JavaApps/src/javaapps/JavaApps.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapps;
+
+/**
+ *
+ * @author barcz
+ */
+public class JavaApps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.runF();
+    }
+    
+}
